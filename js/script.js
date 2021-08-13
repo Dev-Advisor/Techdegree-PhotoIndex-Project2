397384b25eb33baee5be2d97126cb1c1e2d360e0
Macabee51 <monasticculture@gmail.com>
2021-08-12 21:10:50 -0700

    Update Script.js

--- js/script.js
+++ js/script.js
@@ -17,13 +17,64 @@ For assistance:
 Create the `showPage` function
 This function will create and insert/append the elements needed to display a "page" of nine students
 */
+let itemsPerPage = 9;
 
+
+function showPage(list, page) {
+    
+let startIndex =(page * itemsPerPage) - itemsPerPage
+let endIndex = page * itemsPerPage;
+    
+let studentList = document.querySelector('student-list');
+   
+studentList.innerHTML= "";
+   
+
+for(let i=0; i< list.length + itemsPerPage; i++){
+   if(i >= startIndex && i < endIndex){
+   studentList.insertAdjacentHTML( beforeend, `
+   <li class="student-item cf">
+      <div class="student-details">
+         <img class="avatar" src=${list[i].picture.thumbnail}alt="Profile Picture">
+         <h3>${list[i].name.first} ${list[i].name.last}</h3>
+         <span class="email">${list[i].email}/span>
+      </div>
+      <div class="joined-details">
+         <span class="date">Joined ${list[i].registered.date}</span>
+      </div>
+   </li> `);
+         
+         }
+      
+  }
 
 
 /*
 Create the `addPagination` function
 This function will create and insert/append the elements needed for the pagination buttons
 */
+
+function pagination(list){
+   let pageNumber = 5
+   let pageitem = document.querySelector('link-list')
+   pageitem.innerHTML = " ";
+   let button = getElementsByTagName(button);
+  
+   for(i = 0; i > pageNumber; i++){
+      pageitem.insertAdjacentHTML(beforeend, 
+          `
+          <li>
+          <button type="button">1</button>
+         </li>
+         `
+       ); 
+       }
+       pageitem.addEventListener('click', (event) => {
+          button.classlist.remove('active');
+          event = event.target;
+          event.classList.add('active');
+        });
+  }
 
 
 
